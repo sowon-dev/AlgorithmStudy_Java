59041ee6a584b6c0a8b50419f62b9595e8734fae
sowon-dev <sowonkim177@gmail.com>
2020-10-06 10:01:34 +0900

    Create c1022.java

---
+++ src/c1022.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class c1022 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    //띄어쓰기를 포함하여 인식하는 nextLine() 사용.
+		String s = sc.nextLine();
+		System.out.println(s);
+	}
+}
