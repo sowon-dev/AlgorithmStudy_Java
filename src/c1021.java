a819dc1a3fa7bdef9d68b663464659f4b2cd901f
sowon-dev <sowonkim177@gmail.com>
2020-10-06 09:08:23 +0900

    Update c1021.java

--- src/c1021.java
+++ src/c1021.java
@@ -6,7 +6,7 @@ public class c1021 {
 		Scanner sc =  new Scanner(System.in);
 		String c = sc.next();
     
-     //char 배열에 넣어야한다.
+     //char로 받을 수 없으므로 문자열로 받은 뒤 char 배열에 넣.
 		char[] arr = c.toCharArray();
 		System.out.println(arr);
 	}
