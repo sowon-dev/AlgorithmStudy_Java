b5cf32e885cb7317c903d3cc7cae6526c4902a85
sowon-dev <sowonkim177@gmail.com>
2020-10-06 09:07:48 +0900

    Create c1021.java

---
+++ src/c1021.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class c1021 {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		String c = sc.next();
+    
+     //char 배열에 넣어야한다.
+		char[] arr = c.toCharArray();
+		System.out.println(arr);
+	}
+
+}
