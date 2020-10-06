9e5d0b926ad0aeb6a4812adf03014b4c10534870
sowon-dev <sowonkim177@gmail.com>
2020-10-06 15:45:08 +0900

    Create c1023StringTokenizer.java

---
+++ src/c1023StringTokenizer.java
@@ -0,0 +1,22 @@
+public class c1024 {
+
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  String s = sc.next();
+
+  /*
+  //split()사용
+  String[] ss = s.split("");
+
+  for(int i=0; i<ss.length; i++){
+  System.out.println("'"+ss[i]+"'");
+  }*/
+
+  //StringTokenizer 사용
+  StringTokenizer st = new StringTokenizer(s,".");
+
+  while (st.hasMoreElements()) {
+  System.out.println(st.nextToken());
+  }
+
+}
