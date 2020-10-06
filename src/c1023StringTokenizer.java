a3259f2d487182dae5ead0bfdc062e1147de0b8e
sowon-dev <sowonkim177@gmail.com>
2020-10-06 15:47:04 +0900

    Update c1023StringTokenizer.java

--- src/c1023StringTokenizer.java
+++ src/c1023StringTokenizer.java
@@ -1,22 +1,22 @@
 public class c1024 {
 
   public static void main(String[] args) {
-  Scanner sc = new Scanner(System.in);
-  String s = sc.next();
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
 
-  /*
-  //split()사용
-  String[] ss = s.split("");
+    /*
+    //split()사용
+    String[] ss = s.split("");
 
-  for(int i=0; i<ss.length; i++){
-  System.out.println("'"+ss[i]+"'");
-  }*/
+    for(int i=0; i<ss.length; i++){
+    System.out.println("'"+ss[i]+"'");
+    }*/
 
-  //StringTokenizer 사용
-  StringTokenizer st = new StringTokenizer(s,".");
+    //StringTokenizer 사용 (한 개의 구분자만을 사용가능)
+    StringTokenizer st = new StringTokenizer(s,".");
 
-  while (st.hasMoreElements()) {
-  System.out.println(st.nextToken());
+    while (st.hasMoreElements()) {
+    System.out.println(st.nextToken());
   }
 
 }
