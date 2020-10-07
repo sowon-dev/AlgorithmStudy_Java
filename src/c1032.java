75f081d110e26f2d0c2301d0125a64c7db65e07a
sowon-dev <sowonkim177@gmail.com>
2020-10-07 10:09:36 +0900

    Create c1032.java

---
+++ src/c1032.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class c1032 {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+		
+		//16진수
+		System.out.printf("%x", num);		
+	}
+
+}
