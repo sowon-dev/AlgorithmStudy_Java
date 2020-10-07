2aa79ab6175b56e873ec4b42f65de58f194f3784
sowon-dev <sowonkim177@gmail.com>
2020-10-07 12:03:45 +0900

    Create c1035.java

---
+++ src/c1035.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		String s = sc.next();
+		sc.close();
+		
+		//String을 16진수로 변경
+		int tmp = Integer.parseInt(s, 16);
+		
+		//16진수를 8진수로 출력
+		System.out.printf("%o", tmp);		
+	}
+
+}
