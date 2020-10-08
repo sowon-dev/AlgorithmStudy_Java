003aba8824f26c659fabfc913fb7c1533f40f874
sowon-dev <sowonkim177@gmail.com>
2020-10-08 10:43:58 +0900

    Create c1039.java

---
+++ src/c1039.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		String s = sc.nextLine();
+		sc.close();
+		
+		String[] sArr = s.split(" ");
+		
+		//String -> long 출력(int범위를 넘기때문에 long)
+		System.out.printf("%d", Long.parseLong(sArr[0])+Long.parseLong(sArr[1]));
+	}
+}
