bce2a22c3e981d1e391fa21d066df0166d96800b
sowon-dev <sowonkim177@gmail.com>
2020-10-12 17:16:45 +0900

    Create c1059.java

---
+++ src/c1059.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class c1059 {
+  public static void main(String[] args) {
+    Scanner sc =  new Scanner(System.in);
+    int a = sc.nextInt();
+  System.out.println(~a);
+  }
+}
