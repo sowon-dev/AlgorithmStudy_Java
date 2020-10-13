824e9d74ef740fad3a9f28c52bde2ec4065db20d
sowon-dev <sowonkim177@gmail.com>
2020-10-13 10:03:42 +0900

    Create c1069.java

---
+++ src/c1069.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class c1069 {
+
+  public static void main(String[] args) {
+    Scanner sc =  new Scanner(System.in);
+    String a = sc.nextLine();
+    sc.close();
+
+    switch (a) {
+    case "A": 
+      System.out.println("best!!!");
+      break;
+    case "B": 
+      System.out.println("good!!");
+      break;
+    case "C": 
+      System.out.println("run!");
+      break;
+    case "D": 
+      System.out.println("slowly~");
+      break;
+    default:
+      System.out.println("what?");
+    }   
+  }
+}
