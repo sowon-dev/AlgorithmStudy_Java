06c5148973f675a12f7ee8a2ed04769273ddfbcc
sowon-dev <sowonkim177@gmail.com>
2020-10-14 12:45:17 +0900

    Create c1074.java

---
+++ src/c1074.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		sc.close();
+		
+		//첫번째방법
+/*		while(num != 0){
+			System.out.println(num);
+			num--;
+		}*/
+		
+		//두번째방법
+		
+		while(true){
+			if(num == 0) break;
+			System.out.println(num);
+			num--;
+		}
+	}
+}
