83ef568428245f721fe8658efdc52c14c5e8486a
sowon-dev <sowonkim177@gmail.com>
2020-10-19 10:15:45 +0900

    Create c1089.java

---
+++ src/codeup100/c1089.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class c1089 {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		String[] nums = sc.nextLine().split(" ");
+		sc.close();
+		
+		int a = Integer.parseInt(nums[0]);
+		int d = Integer.parseInt(nums[1]);
+		int n = Integer.parseInt(nums[2]);
+		
+		int sum=0;
+		for(int i=0; i<n ; i++){
+			sum = a+(d*i);
+		}
+        System.out.println(sum);
+               
+	}
+}
