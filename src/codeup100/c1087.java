9e3a5951e955ac0086ec8e8dc6d34f0ed4f6fe80
sowon-dev <sowonkim177@gmail.com>
2020-10-19 10:06:03 +0900

    Update c1087.java

--- src/codeup100/c1087.java
+++ src/codeup100/c1087.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class c1087 {
     public static void main(String[] args) {
-		Scanner sc =  new Scanner(System.in);
-        int endNum = sc.nextInt();
-        sc.close();
-        
-        int sum = 0;
-        for(int i=0; ; i++) {
-        	if(endNum <= sum) break;
-        	sum += i;
-        }
-       System.out.println(sum);
+	Scanner sc =  new Scanner(System.in);
+	int endNum = sc.nextInt();
+	sc.close();
+
+	int sum = 0;
+	for(int i=0; ; i++) {
+		if(endNum <= sum) break;
+		sum += i;
+	}
+	System.out.println(sum);
     }
 }
