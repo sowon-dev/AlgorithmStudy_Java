fbade1ff3cd8f7ba7a11ce9264385932a47476b3
sowon <sowonkim177@gmail.com>
2020-11-27 22:46:22 +0900

    issue #5

--- src/leetcodeEasyLevel/TwoSum.java
+++ src/leetcodeEasyLevel/TwoSum.java
@@ -28,5 +28,17 @@ public class TwoSum {
         }
         //return ans;
         System.out.println(Arrays.toString(ans));
+
+        //solution
+        /*
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+         */
     }
 }
