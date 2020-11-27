f701ddfd22fdcac3037e42f80201cafbdd695480
sowon <sowonkim177@gmail.com>
2020-11-27 22:43:21 +0900

    DoneOftwoSum

--- src/leetcodeEasyLevel/TwoSum.java
+++ src/leetcodeEasyLevel/TwoSum.java
@@ -1,7 +1,32 @@
 package leetcodeEasyLevel;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
+        int[] nums;
+        int target;
 
+        //test case1
+        nums = new int[]{3, 2, 4};
+        target = 6;
+
+        //test case2
+        nums = new int[]{2, 7, 11, 15};
+        target = 9;
+
+        int[] ans = new int[2];
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+        //return ans;
+        System.out.println(Arrays.toString(ans));
     }
 }
