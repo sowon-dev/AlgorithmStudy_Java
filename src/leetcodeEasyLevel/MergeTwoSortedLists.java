2e0f0d9422f23f6158b61f65da62afedd31c6ee8
sowon <sowonkim177@gmail.com>
2020-11-30 11:10:45 +0900

    issue #6 21-Done

--- src/leetcodeEasyLevel/MergeTwoSortedLists.java
+++ src/leetcodeEasyLevel/MergeTwoSortedLists.java
@@ -1,44 +1,52 @@
 package leetcodeEasyLevel;
 
 public class MergeTwoSortedLists {
+
+    //leetcode 제출
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    //test cases
     public static void main(String[] args) {
-        Solution sol = new Solution();
 
-        //test cases
-        sol.mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),new ListNode(1, new ListNode(3, new ListNode(4))));
-        //sol.mergeTwoLists(new ListNode(),new ListNode());
-        //sol.mergeTwoLists(new ListNode(),new ListNode(0));
-
-        System.out.println("답: "+sol.toString());
-    }
-}
-
-//leetcode 제출
-class Solution {
-    ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        System.out.println("l1은 "+l1);
-        System.out.println("l2는 "+l2);
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+        System.out.println("답: ["+
+            //mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),new ListNode(1, new ListNode(3, new ListNode(4))))
+            mergeTwoLists(new ListNode(),new ListNode())
+            //mergeTwoLists(new ListNode(),new ListNode(0))
+        +"]");
     }
 }
 
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
+    ListNode() {
+        //공백을 나타내기 위해서 -1로 정의
+        this.val = -1;
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
     @Override
     public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next=" + next +
-                '}';
+        //return "ListNode{" + "val=" + val +", next=" + next + '}';
+        if(next == null) return val +"";
+        return val +","+ next;
     }
 }
