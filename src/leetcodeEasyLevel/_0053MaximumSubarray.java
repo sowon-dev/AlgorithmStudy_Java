c47e8b9a16e3c7bbb7246805596ccc154101d66c
sowon <sowonkim177@gmail.com>
2020-12-25 23:33:18 +0900

    issue #14 Done53

--- src/leetcodeEasyLevel/_0053MaximumSubarray.java
+++ src/leetcodeEasyLevel/_0053MaximumSubarray.java
@@ -3,10 +3,20 @@ package leetcodeEasyLevel;
 public class _0053MaximumSubarray {
 
     static int maxSubArray(int[] nums) {
-        int result = 0;
+        int maxSub = nums[0];
+        int sum = 0;
 
+        for(int n : nums){
 
-        return result;
+            // sum이 음수값이면 고려하지않는다. 다음 n부터 합을 찾는다
+            if(sum < 0 ){
+                sum = 0;
+            }
+
+            sum += n; //-2, 1, -2, -3, 4, 3, 5, 6, -1, 4
+            maxSub = Math.max(maxSub, sum); //-2, 1, 1, 1, 4, 4, 5, 6, 6, 6
+        }
+        return maxSub;
     }
 
     //테스트케이스
