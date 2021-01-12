93b196eea9f45ceb951ef4aeced3cd932d1d4f96
sowon <sowonkim177@gmail.com>
2021-01-12 23:47:06 +0900

    issue #9 141-ing

---
+++ src/leetcodeEasyLevel/_0141/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package leetcodeEasyLevel._0141;
+
+public class LinkedListCycle {
+
+  public boolean hasCycle(ListNode head) {
+
+  }
+}
+
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
