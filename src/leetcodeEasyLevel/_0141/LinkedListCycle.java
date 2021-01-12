ebb3b5496cec117c124fac3e173e0838fe83cfc4
sowon <sowonkim177@gmail.com>
2021-01-13 00:00:59 +0900

    issue #10 141-Done

--- src/leetcodeEasyLevel/_0141/LinkedListCycle.java
+++ src/leetcodeEasyLevel/_0141/LinkedListCycle.java
@@ -2,9 +2,27 @@ package leetcodeEasyLevel._0141;
 
 public class LinkedListCycle {
 
-  public boolean hasCycle(ListNode head) {
+  public static boolean hasCycle(ListNode head) {
+    //엣지케이스
+    if (head == null) {
+      return false;
+    }
+    //slow는 한 칸씩 이동
+    //fast는 두 칸씩 이동
+    ListNode slow = head;
+    ListNode fast = head.next;
 
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      // 포인터끼리 만나면 true
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
   }
+
 }
 
 class ListNode {
