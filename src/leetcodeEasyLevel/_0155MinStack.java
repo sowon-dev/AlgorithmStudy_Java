bd0087668065668f159339c34c88a5c6474ef9a0
sowon <sowonkim177@gmail.com>
2021-01-15 00:22:19 +0900

    issue #10 155-Done

--- src/leetcodeEasyLevel/_0155MinStack.java
+++ src/leetcodeEasyLevel/_0155MinStack.java
@@ -1,24 +1,32 @@
 package leetcodeEasyLevel;
 
-public class _0155MinStack {
-  /** initialize your data structure here. */
-  public MinStack() {
+import java.util.Stack;
 
-  }
+public class _0155MinStack {
+  // Runtime 5 ms Memory 40.6 MB
+  //스택을 두 개 생성하고 한 스택에는 min_val만 담는다.
+    Stack<Integer> s = new Stack();
+    Stack<Integer> min = new Stack();
 
   public void push(int x) {
-
+    if(min.isEmpty() || x <= min.peek()){
+      min.push(x);
+    }
+    s.push(x);
   }
 
   public void pop() {
-
+    if(s.peek().equals(min.peek())){
+      min.pop();
+    }
+      s.pop();
   }
 
   public int top() {
-
+      return s.peek();
   }
 
   public int getMin() {
-
+    return min.peek();
   }
 }
