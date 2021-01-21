7c00eaeefb650f81c3c0888809155dced4b1bd32
sowon <sowonkim177@gmail.com>
2021-01-21 23:48:32 +0900

    issue #11 190-Done

---
+++ src/leetcodeEasyLevel/_0190ReverseBits.java
@@ -0,0 +1,18 @@
+package leetcodeEasyLevel;
+
+public class _0190ReverseBits {
+  // you need treat n as an unsigned value
+  public int reverseBits(int n) {
+    int ret=0;
+    // because there are 32 bits in total
+    for (int i = 0; i <32;i++){
+      ret = ret<<1;
+      // If the bit is 1 we OR it with 1, ie add 1
+      if((n & 1) > 0){
+        ret = ret | 1;
+      }
+      n=n>>>1;
+    }
+    return ret;
+  }
+}
