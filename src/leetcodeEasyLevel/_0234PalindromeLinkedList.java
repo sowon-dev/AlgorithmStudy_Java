068d6fbdba4a1d4de4995180e1ed07388c5bfae6
sowon <sowonkim177@gmail.com>
2021-01-28 23:51:12 +0900

    issue #17 237-Ing

--- src/leetcodeEasyLevel/_0234PalindromeLinkedList.java
+++ src/leetcodeEasyLevel/_0234PalindromeLinkedList.java
@@ -1,24 +1,36 @@
 package leetcodeEasyLevel;
 
 public class _0234PalindromeLinkedList {
-  protected ListNode reverse(ListNode head){
-    if (head == null || head.next == null)
-      return head;
-    ListNode next = reverse(head.next);
-    head.next.next = head;
-    head.next = null;
-    return next;
-  }
-  public boolean isPalindrome(ListNode head) {
-    ListNode fast = head, slow = head;
+  // Runtime 2 ms Memory 44.8 MB
+  // 리스트가 팰린드롬이면 true를, 아니면 false를 리턴하는 문제이다.
+  // 팰린드롬이란? https://sowon-dev.github.io/2021/01/09/210110replacevsreplaceall/
+  // 1->2->2->1
 
-    while(fast != null && fast.next != null){
-      fast = fast.next.next;
-      slow = slow.next;
+  private ListNode reverse(ListNode head){
+    ListNode preNode = null;
+    while(head != null){
+      ListNode nextNode = head.next;
+      head.next = preNode;
+      preNode = head;
+      head = nextNode;
+    }
+    return preNode;
+  }
+
+  public boolean isPalindrome(ListNode head) {
+    // ListNode에 null만 있어도 팰린드롬이다.
+    if (head == null || head.next == null) return true;
+
+    ListNode slow = head; // 1
+    ListNode fast = head; // 1
+
+    while(fast != null && fast.next != null) { // 1!=2 2!=2 null&&null => while종료
+      slow = slow.next; //2 2
+      fast = fast.next.next; //2 null
     }
 
-    fast = head;
-    slow = reverse(slow);
+    fast = head; // 1 2 2 null
+    slow = reverse(slow); //역순으로 1 2 null
 
     while (fast != null && slow != null){
       if (fast.val != slow.val)
---
+++ src/leetcodeEasyLevel/_237DeleteNodeinaLinkedList.java
@@ -0,0 +1,8 @@
+package leetcodeEasyLevel;
+
+public class _237DeleteNodeinaLinkedList {
+  public void deleteNode(ListNode node) {
+    //단순연결리스트에서 주어진 node를 삭제하고 출력하는 문제이다.
+    // Runtime  Memory
+  }
+}
