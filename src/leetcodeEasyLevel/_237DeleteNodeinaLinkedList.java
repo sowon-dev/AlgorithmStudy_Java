a8366d9e58bfe529fe3b6beb71c72fea621a0989
sowon <sowonkim177@gmail.com>
2021-01-30 23:54:31 +0900

    issue #17 237-Ing

--- src/leetcodeEasyLevel/_237DeleteNodeinaLinkedList.java
+++ src/leetcodeEasyLevel/_237DeleteNodeinaLinkedList.java
@@ -4,5 +4,7 @@ public class _237DeleteNodeinaLinkedList {
   public void deleteNode(ListNode node) {
     //단순연결리스트에서 주어진 node를 삭제하고 출력하는 문제이다.
     // Runtime  Memory
+    node.val = node.next.val;
+    node.next = node.next.next;
   }
 }
