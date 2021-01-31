e7def51058aca93b87a0ce3f6d87e965135100a5
sowon <sowonkim177@gmail.com>
2021-01-31 12:20:13 +0900

    1080-done

--- src/codeup100/c1080.java
+++ src/codeup100/c1080.java
@@ -1,29 +1,53 @@
 package codeup100;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class c1080 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //sol1 Memory 14952 Runtime 113
+        /*
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         sc.close();
 
         int sum = 0;
         int i;
-
-        //내코드
-/*
         for(i = 1; sum < num; i++){
             sum += i;
         }
         System.out.println(i-1);
-*/
+        */
 
-        //두번째코드
-        for(i=1; i<num; i++){
-            sum += i;
-            if (sum >= num ) break;
+        //sol2
+        // Using sc.close()  :  Memory 14348 Runtime 114
+        // Not using sc.close() : Memory 14312 Runtime 112
+        /*
+        Scanner sc = new Scanner(System.in);
+        int sum = sc.nextInt();
+        sc.close();
+
+        int tempSum = 0;
+        int i = 0;
+        for(i=0;  tempSum < sum; i++){
+          tempSum += i;
+          if(tempSum >= sum) break;
         }
         System.out.println(i);
+        */
+
+        //sol3 : use BufferReader
+        //Memory 11128 Runtime 66
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = Integer.parseInt(br.readLine());
+        int tempSum = 0;
+        int j = 0;
+        for(j=0;  tempSum < sum; j++){
+            tempSum += j;
+            if(tempSum >= sum) break;
+        }
+        System.out.println(j);
     }
 }
