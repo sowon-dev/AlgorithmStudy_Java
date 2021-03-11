612cdd27f3d2e53710184dba5f9c668970e5bd86
sowon <sowonkim177@gmail.com>
2021-03-11 23:58:10 +0900

    issue #28 Grid Challenge w/ Explanation

--- src/hackerrank/GridChallenge.java
+++ src/hackerrank/GridChallenge.java
@@ -3,36 +3,37 @@ package hackerrank;
 import java.util.Arrays;
 
 public class GridChallenge {
+
   static String gridChallenge(String[] grid) {
-    char ll[] = new char[grid.length];
-    char b[] = new char[grid.length];
-    boolean xx=true;
-    int i=0;
-    while(xx && i<grid[0].length()){
-      for(int j=0 ; j<grid.length;j++ ){
-        ll[j]=grid[j].charAt(i);
-      }
+    //그리드가 열이 오름차순 정렬이면 YES출력하는 문제
 
-      b= Arrays.copyOf(ll,grid.length);
-      Arrays.sort(ll);
+    char charArr[] = new char[grid.length];
+    char copyCharArr[] = new char[grid.length];
+    boolean isAscending = true;
 
-      if(!(Arrays.equals(b,ll))){
-        xx=false;
-        break;
-      }
-      i++;
-    }
+    for (int i = 0; i < grid.length; i++) {
+      //주어진 grid를 char arr로 바꾼뒤 오름차순 정렬한다.
+      char[] chars = grid[i].toCharArray();
+      System.out.println("char arr: "+Arrays.toString(chars));
+      Arrays.sort(chars);
 
-    if(xx){
-      return "YES";
+      //정렬된 char arr를 String arr로 만든다
+      grid[i] = new String(chars);
+      System.out.println("grid[i]: "+Arrays.toString(grid));
+
+      //compareTo
+      if ( i != 0 && grid[i].compareTo(grid[i-1]) < 0 )
+      System.out.println("compareTo: "+ (grid[i].compareTo(grid[i-1]) < 0) );
+        return "NO";
     }
-    else{
-      return "NO";
-    }
+    return "YES";
   }
 
   public static void main(String[] args) {
-    System.out.println(gridChallenge(new String[]{"abc", "ade", "efg"})+", ans:YES");
-    System.out.println(gridChallenge(new String[]{"ebacd", "fghij", "olmkn", "trpqs", "xywuv"})+", ans:YES");
+//    System.out.println(gridChallenge(new String[]{"abc", "ade", "efg"}) + ", ans:YES");
+//    System.out.println(gridChallenge(new String[]{"ebacd", "fghij", "olmkn", "trpqs", "xywuv"}) + ", ans:YES");
+    System.out.println(gridChallenge(new String[]{"kc", "iu"}) + ", ans:YES");
+//    System.out.println(gridChallenge(new String[]{"uxf", "vof", "hmp"}) + ", ans:NO");
+//    System.out.println(gridChallenge(new String[]{"ppp", "ypp", "wyw"}) + ", ans:YES");
   }
 }
