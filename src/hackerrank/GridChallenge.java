ca1c5a053448158597861043d92655fbf3ea47a3
sowon <sowonkim177@gmail.com>
2021-03-14 09:41:55 +0900

    issue #28 Grid Challenge java w/ Explanation

--- src/hackerrank/GridChallenge.java
+++ src/hackerrank/GridChallenge.java
@@ -5,35 +5,41 @@ import java.util.Arrays;
 public class GridChallenge {
 
   static String gridChallenge(String[] grid) {
-    //그리드가 열이 오름차순 정렬이면 YES출력하는 문제
+    // 그리드의 열이 사전순 정렬이면 YES를 출력하는 문제
+    // 단, 그리드의 행은 순서를 변경할 수 있다.
 
-    char charArr[] = new char[grid.length];
-    char copyCharArr[] = new char[grid.length];
-    boolean isAscending = true;
-
+    // 1. 행을 사전순 정렬하기
+    // String을 char로 변환하여 사전순으로 sort한 후 다시 grid[i]번째에 넣는다.
+    System.out.println(Arrays.toString(grid));
     for (int i = 0; i < grid.length; i++) {
-      //주어진 grid를 char arr로 바꾼뒤 오름차순 정렬한다.
       char[] chars = grid[i].toCharArray();
-      System.out.println("char arr: "+Arrays.toString(chars));
       Arrays.sort(chars);
-
-      //정렬된 char arr를 String arr로 만든다
-      grid[i] = new String(chars);
-      System.out.println("grid[i]: "+Arrays.toString(grid));
-
-      //compareTo
-      if ( i != 0 && grid[i].compareTo(grid[i-1]) < 0 )
-      System.out.println("compareTo: "+ (grid[i].compareTo(grid[i-1]) < 0) );
-        return "NO";
+      grid[i] = String.valueOf(chars);
     }
+    System.out.println(Arrays.toString(grid));
+
+    // 2. 열을 사전순 정렬인지 확인하기
+    // 지금 열과 다음 열의 i번째를 비교하여 사전순이 아니면 NO를 출력
+    for (int i = 0; i < grid[0].length(); i++) {
+      for (int j = 0; j < grid.length - 1; j++) {
+        System.out.println(grid[j].charAt(i)+", "+grid[j + 1].charAt(i));
+        if (grid[j].charAt(i) > grid[j + 1].charAt(i)) {
+          return "NO";
+        }
+      }
+    }
+
     return "YES";
   }
 
   public static void main(String[] args) {
-//    System.out.println(gridChallenge(new String[]{"abc", "ade", "efg"}) + ", ans:YES");
+    System.out.println(gridChallenge(new String[]{"abc", "ade", "efg"}) + ", ans:YES");
 //    System.out.println(gridChallenge(new String[]{"ebacd", "fghij", "olmkn", "trpqs", "xywuv"}) + ", ans:YES");
-    System.out.println(gridChallenge(new String[]{"kc", "iu"}) + ", ans:YES");
-//    System.out.println(gridChallenge(new String[]{"uxf", "vof", "hmp"}) + ", ans:NO");
+//    System.out.println(gridChallenge(new String[]{"kc", "iu"}) + ", ans:YES");
+    System.out.println(gridChallenge(new String[]{"uxf", "vof", "hmp"}) + ", ans:NO");
 //    System.out.println(gridChallenge(new String[]{"ppp", "ypp", "wyw"}) + ", ans:YES");
+//    System.out.println(gridChallenge(new String[]{"eibjbwsp", "ptfxehaq", "jxipvfga", "rkefiyub", "nflvjznh"}) + ", ans:NO");
+//    System.out.println(gridChallenge(new String[]{"kalwfhfj", "lktajiaq", "srdgoros"}) + ", ans:YES");
+
   }
 }
