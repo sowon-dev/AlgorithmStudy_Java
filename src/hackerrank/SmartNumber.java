4757ac805e9918f9ded637be95e34b93e158b984
sowon <sowonkim177@gmail.com>
2021-03-28 02:51:22 +0900

    issue #30 SmartNumber

--- src/hackerrank/SmartNumber.java
+++ src/hackerrank/SmartNumber.java
@@ -2,13 +2,19 @@ package hackerrank;
 
 public class SmartNumber {
   public static boolean isSmartNumber(int num) {
+    // 주어진 수의 제곱근의 갯수가 홀수면 YES, 짝수면 NO를 출력하는 문제
+    // 조건: 주어진 코드중 한 줄만 바꿀 것
     int val = (int) Math.sqrt(num);
-    if(num / val == 1)
+    if(val*val == num)
       return true;
     return false;
   }
 
   public static void main(String[] args) {
-    System.out.println(isSmartNumber(4));
+    System.out.println(isSmartNumber(1)+", true=Yes=홀수");
+    System.out.println(isSmartNumber(2)+", false=NO=짝수");
+    System.out.println(isSmartNumber(7)+", false=NO=짝수");
+    System.out.println(isSmartNumber(9)+", true=YES=홀수");
+    System.out.println(isSmartNumber(169)+", true=YES=홀수");
   }
 }
