8342b02be458bd9706ebfac5f398937a153f5d08
sowon <sowonkim177@gmail.com>
2021-05-21 00:13:24 +0900

    issue #37 네 번째 점

--- src/backjoon/_3009.java
+++ src/backjoon/_3009.java
@@ -4,45 +4,34 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class _3009 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //sol memory  runtime
+    // sol memory 11452 runtime 76
+    // 직사각형이 되기 위해서는 쌍을 이루지 않는 숫자를 찾으면 됨
     String[] coord_1 = br.readLine().split(" ");
     String[] coord_2 = br.readLine().split(" ");
     String[] coord_3 = br.readLine().split(" ");
 
     String x;
     String y;
-
-    // x 좌표 비교
-    // 1번 x좌표와 2번 x좌표 비교
+    
+    // x 좌표 비교해서 쌍이 없는 x좌표 찾기
     if(coord_1[0].equals(coord_2[0])){
       x = coord_3[0];
-    }
-
-    // 1번 x좌표와 3번 x좌표 비교
-    else if (coord_1[0] == coord_3[0]) {
+    } else if (coord_1[0].equals(coord_3[0])) {
       x = coord_2[0];
-    }
-    // 2번 x좌표와 3번 x좌표 비교
-    else {
+    } else {
       x = coord_1[0];
     }
 
-    // y 좌표 비교
-    // 1번 x좌표와 2번 x좌표 비교
-    if (coord_1[1] == coord_2[1]) {
+    // y 좌표 비교해서 쌍이 없는 y좌표 찾기
+    if (coord_1[1].equals(coord_2[1])) {
       y = coord_3[1];
-    }
-    // 1번 x좌표와 3번 x좌표 비교
-    else if (coord_1[1] == coord_3[1]) {
+    } else if (coord_1[1].equals(coord_3[1])) {
       y = coord_2[1];
-    }
-    // 2번 x좌표와 3번 x좌표 비교
-    else {
+    } else {
       y = coord_1[1];
     }
 
