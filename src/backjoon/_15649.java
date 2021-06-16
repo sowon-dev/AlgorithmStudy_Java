28dc189441f4bfd9a2b65ad26717d3cd38877992
sowon <sowonkim177@gmail.com>
2021-06-17 07:25:45 +0900

    issue #41 N과 M (1)

--- src/backjoon/_15649.java
+++ src/backjoon/_15649.java
@@ -1,5 +1,6 @@
 package backjoon;
-
+// https://www.acmicpc.net/problem/15649
+// N과 M (1)
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,9 +13,9 @@ public class _15649 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // memory 19912 runtime 156
     StringTokenizer st = new StringTokenizer(br.readLine());
-    // memory  runtime
-    int N = Integer.parseInt(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
 
     arr = new int[M];
@@ -24,6 +25,7 @@ public class _15649 {
   }
 
   public static void dfs(int N, int M, int depth) {
+    // arr배열의 길이(depth)가 M과 같다면 출력
     if (depth == M) {
       for (int val : arr) {
         sb.append(val).append(' ');
@@ -32,13 +34,32 @@ public class _15649 {
       return;
     }
 
+    // 중복값 체크를 위한 visit배열
     for (int i = 0; i < N; i++) {
-      if (!visit[i]) {
+      if (!visit[i]) { // 사용여부판단
         visit[i] = true;
         arr[depth] = i + 1;
-        dfs(N, M, depth + 1);
-        visit[i] = false;
+        dfs(N, M, depth + 1); // 재귀호출
+        visit[i] = false; //하나의 재귀함수가 끝나면 false로 바꿔줘야 다시 i라는 숫자 사용가능
       }
     }
   }
 }
+/*
+INPUT
+4 2
+
+OUTPUT
+1 2
+1 3
+1 4
+2 1
+2 3
+2 4
+3 1
+3 2
+3 4
+4 1
+4 2
+4 3
+ */
