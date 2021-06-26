44e6181cf43bdd6200c8b8bc7348a5fcc68f8e54
sowon <sowonkim177@gmail.com>
2021-06-26 23:59:17 +0900

    d

---
+++ src/backjoon/_14889.java
@@ -0,0 +1,13 @@
+package backjoon;
+// https://www.acmicpc.net/problem/14889
+// 스타트와 링크
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14889 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // memory  runtime
+  }
+}
